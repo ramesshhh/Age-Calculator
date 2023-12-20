47f144c5ea8f4e9451827a1a3245a49699895e28
rameshhh <rameshjha55061@gmail.com>
2023-12-20 10:35:19 +0530

    20-12-2023-Part-1

--- Project-2/test.js
+++ Project-2/test.js
@@ -101,7 +101,9 @@ $(".slider").slick({
     <!--  -->
 
 */
-<script>
+
+/* 
+    <script>
 $('.slider').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
@@ -118,3 +120,57 @@ $('.slider').slick({
                 <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
+
+*/
+
+
+
+/*
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!-- Slider code 1st starts -->
+
+<link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick-theme.css"/>
+
+<!-- Slider code 1st ends -->
+
+
+<!-- Slider code 2nd start { genesis_after Hook } -->
+
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="https://kenwheeler.github.io/slick/slick/slick.min.js"></script>
+
+    <script>
+        $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        });
+    </script>
+
+<!-- Slider code 2nd ends -->
+
+<!-- slider code 3rd starts In Homepage -->
+
+    <script>
+        $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        });
+    </script>
+    <div class="slider">
+      <div>your content</div>
+      <div>your content2</div>
+      <div>your content3</div>
+    </div>
+
+<!-- slider code 3rd ends In Homepage -->
+ */
